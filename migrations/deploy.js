01ddf22797684f79a75cfc05c3d1b5f52fc5ba15
Mehboob01 <mehboobelahi661@gmail.com>
2023-07-08 17:40:23 +0500

    first commit

--- migrations/deploy.js
+++ migrations/deploy.js
@@ -1,3 +1,7 @@
+/**
+ * @dev Deploy the SeekCoin contract using Truffle.
+ * The SeekCoin contract will be deployed to the blockchain.
+ */
 const SeekCoin = artifacts.require("SeekCoin");
 
 module.exports = function (deployer) {
